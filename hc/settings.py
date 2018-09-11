972995eec69d50a3dfe68aa2aa4974091982b6b9
Mithun Arunan <mithun1848@gmail.com>
2018-09-11 22:58:16 +0530

    fix: set ALLOWED_HOSTS to *

--- hc/settings.py
+++ hc/settings.py
@@ -17,11 +17,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 HOST = "localhost"
 SECRET_KEY = "---"
-DEBUG = True
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ["*"]
 DEFAULT_FROM_EMAIL = 'healthchecks@example.org'
 USE_PAYMENTS = False
-REGISTRATION_OPEN = True
+REGISTRATION_OPEN = False
 
 
 INSTALLED_APPS = (
