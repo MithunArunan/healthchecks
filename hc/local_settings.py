29d452be78625100d16f9e5dfc1a19d35945ff11
Mithun Arunan <mithun1848@gmail.com>
2020-03-16 18:52:52 +0530

    add: email settings env

--- hc/local_settings.py
+++ hc/local_settings.py
@@ -1,4 +1,12 @@
 import os
+import warnings
+
+def envbool(s, default):
+    v = os.getenv(s, default=default)
+    if v not in ("", "True", "False"):
+        msg = "Unexpected value %s=%s, use 'True' or 'False'" % (s, v)
+        raise Exception(msg)
+    return v == "True"
 
 DATABASES = {
     'default': {
@@ -15,3 +23,12 @@ SLACK_CLIENT_ID = os.environ['SLACK_CLIENT_ID']
 SLACK_CLIENT_SECRET = os.environ['SLACK_CLIENT_SECRET']
 
 SITE_ROOT = os.environ['SITE_ROOT']
+
+EMAIL_HOST = os.environ["EMAIL_HOST"]
+EMAIL_PORT = int(os.environ["EMAIL_PORT"])
+EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
+EMAIL_USE_TLS = envbool("EMAIL_USE_TLS", "True")
+EMAIL_USE_VERIFICATION = envbool("EMAIL_USE_VERIFICATION", "True")
+
+DEFAULT_FROM_EMAIL = os.environ["DEFAULT_FROM_EMAIL"]
